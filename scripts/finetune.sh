c711ca5fac9ab3734fa1899fdc895b1e324e8bcf
yl <2317676993@qq.com>
2024-01-27 23:28:18 +0800

    vary

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -1,15 +1,15 @@
 # --model_name_or_path ./ckpts/llava-opt-v1.0-125m-pretrain\ \
 
 deepspeed   --include "localhost:0,2,3,4" vary/train/train_opt.py\
-            --deepspeed zero_config/zero2.json\
-            --model_name_or_path ./ckpts/llava-opt-v1.0-125m-pretrain \
+            --deepspeed zero_config/zero3.json\
+            --model_name_or_path facebook/opt-125m \
             --conversation_version conversation\
             --freeze_vision_tower True\
             --freeze_lm_model False\
             --use_im_start_end True\
             --bf16 True\
-            --per_device_eval_batch_size 4\
-            --gradient_accumulation_steps 8\
+            --per_device_eval_batch_size 1\
+            --gradient_accumulation_steps 4\
             --evaluation_strategy "no"\
             --save_strategy "steps"\
             --save_steps 5000\
@@ -23,7 +23,7 @@ deepspeed   --include "localhost:0,2,3,4" vary/train/train_opt.py\
             --gradient_checkpointing True\
             --dataloader_num_workers 4\
             --report_to none\
-            --per_device_train_batch_size 2\
+            --per_device_train_batch_size 4\
             --num_train_epochs 1\
             --learning_rate 5e-5\
             --datasets  llava_v1.5_finetune\
